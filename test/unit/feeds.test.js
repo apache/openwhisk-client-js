5d6b2143ad873d61276dfe3333d5de479afbbd7e
James Thomas <james.thomas@uk.ibm.com>
2017-03-28 16:43:17 +0100

    Fixing namespace bug with creating feeds.

--- test/unit/feeds.test.js
+++ test/unit/feeds.test.js
@@ -121,19 +121,19 @@ test('should be able to create trigger using options namespace', t => {
   const client = {}
   client.options = { api_key } 
 
-  const ns = '_'
+  const ns = 'custom'
   const feeds = new Feeds(client)
 
   client.request = (method, path, options) => {
     t.is(method, 'POST')
     t.is(path, `namespaces/${ns}/actions/${feed_name}`)
     t.deepEqual(options.qs, {blocking: true})
-    t.deepEqual(options.body, {authKey: client.options.api_key, lifecycleEvent: 'CREATE', triggerName: `/ns/${trigger_name}`})
+    t.deepEqual(options.body, {authKey: client.options.api_key, lifecycleEvent: 'CREATE', triggerName: `/_/${trigger_name}`})
   }
 
   t.plan(4)
 
-  return feeds.create({name: feed_name, namespace: 'ns', trigger: trigger_name})
+  return feeds.create({name: feed_name, namespace: ns, trigger: trigger_name})
 })
 
 test('should be able to create trigger ignoring options namespace', t => {
@@ -148,7 +148,7 @@ test('should be able to create trigger ignoring options namespace', t => {
 
   client.request = (method, path, options) => {
     t.is(method, 'POST')
-    t.is(path, `namespaces/${ns}/actions/${feed_name}`)
+    t.is(path, `namespaces/ns/actions/${feed_name}`)
     t.deepEqual(options.qs, {blocking: true})
     t.deepEqual(options.body, {authKey: client.options.api_key, lifecycleEvent: 'CREATE', triggerName: `${trigger_name}`})
   }
@@ -179,7 +179,6 @@ test('should be able to create trigger from full qualified feed', t => {
 
   return feeds.create({name: feed_name, namespace: 'ns', trigger: trigger_name})
 })
-
 
 test('should be able to create feed using feedName with params', t => {
   const feed_name = 'feed_name'
