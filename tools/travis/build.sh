54aee461f739de0a1d124661fc6f511d7e358423
Angela Lloyd <alloyd@Angelas-MacBook-Pro.local>
2017-08-03 09:38:00 -0500

    change cli to curl for integration script

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -10,7 +10,7 @@ WHISKDIR="$ROOTDIR/../openwhisk"
 # Install OpenWhisk
 cd $WHISKDIR/ansible
 
-ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=openwhisk"
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=openwhisk "
 
 $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
@@ -19,29 +19,21 @@ $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD apigateway.yml
 
 cd $WHISKDIR
-./gradlew :tools:cli:distDocker -PdockerImagePrefix=openwhisk
-
+ ./gradlew  -PdockerImagePrefix=openwhisk
 cd $WHISKDIR/ansible
 
 $ANSIBLE_CMD wipe.yml
-$ANSIBLE_CMD openwhisk.yml
+$ANSIBLE_CMD openwhisk.yml  -e '{"openwhisk_cli":{"installation_mode":"remote","remote":{"name":"OpenWhisk_CLI","dest_name":"OpenWhisk_CLI","location":"https://github.com/apache/incubator-openwhisk-cli/releases/download/latest"}}}'
 $ANSIBLE_CMD postdeploy.yml
 
 cd $WHISKDIR
 cat whisk.properties
 
-# Set Environment
-export OPENWHISK_HOME=$WHISKDIR
-
-# Set up CLI tool used by prereq script
-sudo cp $WHISKDIR/bin/wsk /usr/bin/wsk
-
 edgehost=$(cat $WHISKDIR/whisk.properties | grep edge.host= | sed s/edge\.host=//)
-wsk property set --apihost $edgehost
-wsk property set --auth "$(cat $WHISKDIR/ansible/files/auth.guest)"
+key=$(cat $WHISKDIR/ansible/files/auth.guest)
 
 # Test
 cd $ROOTDIR
 npm install --dev
 npm run code-coverage-build
-npm run code-coverage-run travis
+npm run code-coverage-run $key $edgehost guest true "travis,insecure"
