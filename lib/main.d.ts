3755a780ebfaced05bd0bfc96db37f4c35039174
Guillaume Baudart <guillaume.baudart@ibm.com>
2017-09-14 09:27:09 -0400

    Requested changes

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,3 +1,6 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
 import * as Swagger from 'swagger-schema-official'
 
 export = openwhisk;
@@ -272,7 +275,7 @@ declare namespace openwhisk {
         "GET" |
         "POST" |
         "PUT" |
-        "PACTH" |
+        "PATCH" |
         "DELETE"
 
     interface Api {
