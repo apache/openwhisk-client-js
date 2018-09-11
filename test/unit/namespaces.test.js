29344973dd0960f533f317cedf99726ce49083a0
Nick Mitchell <starpit@users.noreply.github.com>
2018-09-11 09:45:25 -0400

    fix missing pass-through of user-agent in namespace.list and .get, and activation.get (#136)
    also add a noUserAgent option, to prevent assigning a default user-agent
    
    Fixes #135

--- test/unit/namespaces.test.js
+++ test/unit/namespaces.test.js
@@ -4,6 +4,7 @@
 'use strict'
 
 const test = require('ava')
+const Client = require('../../lib/client')
 const Namespaces = require('../../lib/namespaces')
 
 test('should list all namespaces', t => {
@@ -16,6 +17,80 @@ test('should list all namespaces', t => {
 
   const namespaces = new Namespaces(client)
   return namespaces.list()
+})
+
+test('should list all namespaces, passing through user-agent header', t => {
+  t.plan(3)
+  const client = {}
+  const userAgent = 'userAgentShouldPassThroughPlease'
+  client.request = (method, path, options) => {
+    t.is(method, 'GET')
+    t.is(path, `namespaces`)
+    t.is(options['User-Agent'], userAgent)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.list({'User-Agent': userAgent})
+})
+
+test('should list all namespaces, NOT passing through user-agent header (variant 1)', t => {
+  t.plan(3)
+  const client = {}
+  const userAgent = 'userAgentShouldPassThroughPlease'
+  client.request = async (method, path, options) => {
+    t.is(method, 'GET')
+    t.is(path, `namespaces`)
+
+    const parms = await new Client({api: 'aaa', api_key: 'aaa'}).params(method, path, options)
+    t.is(parms.headers['User-Agent'], undefined)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.list({'User-Agent': userAgent, noUserAgent: true})
+})
+
+test('should list all namespaces, NOT passing through user-agent header (variant 2)', t => {
+  t.plan(3)
+  const client = {}
+  const userAgent = 'userAgentShouldPassThroughPlease'
+  client.request = async (method, path, options) => {
+    t.is(method, 'GET')
+    t.is(path, `namespaces`)
+
+    const parms = await new Client({api: 'aaa', api_key: 'aaa', noUserAgent: true}).params(method, path, options)
+    t.is(parms.headers['User-Agent'], undefined)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.list({'User-Agent': userAgent})
+})
+
+test('should list all namespaces, NOT passing through user-agent header (variant 3)', t => {
+  t.plan(3)
+  const client = {}
+  client.request = async (method, path, options) => {
+    t.is(method, 'GET')
+    t.is(path, `namespaces`)
+
+    const parms = await new Client({api: 'aaa', api_key: 'aaa', noUserAgent: true}).params(method, path, options)
+    t.is(parms.headers['User-Agent'], undefined)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.list({})
+})
+
+test('should list all namespaces, NOT passing through user-agent header (variant 4)', t => {
+  t.plan(3)
+  const client = {}
+  client.request = (method, path, options) => {
+    t.is(method, 'GET')
+    t.is(path, `namespaces`)
+    t.is(options['User-Agent'], undefined)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.list({noUserAgent: true})
 })
 
 test('should retrieve namespace entities', t => {
@@ -31,4 +106,21 @@ test('should retrieve namespace entities', t => {
 
   const namespaces = new Namespaces(client)
   return namespaces.get()
+})
+
+test('should retrieve namespace entities, passing through user-agent header', t => {
+  t.plan(20)
+  const client = {}
+  const userAgent = 'userAgentShouldPassThroughPlease'
+  client.request = (method, path, options) => {
+    t.is(method, 'GET')
+    let parts = path.split('/')
+    t.is(parts[0], 'namespaces')
+    t.is(parts[1], '_')
+    t.is(['actions', 'triggers', 'rules', 'packages'].indexOf(parts[2]) > -1, true)
+    t.is(options['User-Agent'], userAgent)
+  }
+
+  const namespaces = new Namespaces(client)
+  return namespaces.get({'User-Agent': userAgent})
 })
