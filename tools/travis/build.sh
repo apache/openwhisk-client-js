f1c9c8549271672eaaace143fc0a4a3524ad8c48
Shazron Abdullah <shaz@adobe.com>
2019-03-11 12:31:07 +0800

    Fix scancode errors in shell scripts

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+# Licensed to the Apache Software Foundation (ASF) under one or more contributor
+# license agreements; and to You under the Apache License, Version 2.0.
 
 # Build script for Travis-CI.
 
